1dd4d035d87c56bc2bb1fe88ef0586ec7c5e0acc
Ramires Viana <59319979+ramiresviana@users.noreply.github.com>
2020-06-17 19:42:59 +0000

    fix: add button

--- app/Views/listing.php
+++ app/Views/listing.php
@@ -7,7 +7,7 @@
     <h1>Hello HTML</h1>
 <?php if (session()->get('logged')): ?>
     Logged as admin <div class="admin-actions"><a href="<?= base_url('logout') ?>"><button class="red">Logout</button></a></div>
-    There are 100 posts <div class="admin-actions"><a href="<?= base_url('add') ?>"><button>Add new</button></a></div>
+    There are 100 posts <div class="admin-actions"><a href="<?= base_url('new') ?>"><button>Add new</button></a></div>
 <?php endif ?>
 </header>
 <main>

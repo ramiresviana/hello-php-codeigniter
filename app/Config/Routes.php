ac77a09ae2600ccc799c01c606eaca41491a9e7f
Ramires Viana <59319979+ramiresviana@users.noreply.github.com>
2020-06-17 15:50:55 +0000

    fix: article route uri parameter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Article::index');
-$routes->get('/article/(:num)', 'Article::article/1');
+$routes->get('/article/(:num)', 'Article::article/$1');
 $routes->add('/login', 'Article::login');
 $routes->add('/new', 'Article::new');
 
